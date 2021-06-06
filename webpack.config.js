b9f1dec65e2157cb6d03c4ac79a5331cf88f6c67
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-06-06 05:11:17 +0300

    Base href

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ sharedMappings.register(
 module.exports = {
   output: {
     uniqueName: "mf2Payments",
-    publicPath: "/payments/"
+    publicPath: "auto"
   },
   optimization: {
     runtimeChunk: false
